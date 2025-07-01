bd4ab183e0784b10c4db38f0eaefce3d028ffdda
Jeffrey Rowlings <rawlinzjeff264@gmail.com>
2025-07-01 22:56:14 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Home6Banner from '@/components/banner/Home6Banner'
 import Home6BlogSection from '@/components/blog-section/Home6BlogSection'
 import Home6Faq from '@/components/faq-section/Home6Faq'
 import Home6FeatureSection from '@/components/feature-section/Home6FeatureSection'
-import Footer6 from '@/components/Footer/Footer6'
+// import Home1Footer from '@/components/Footer/Home1Footer'
 import Header6 from '@/components/header/Header6'
 import Home6IntegrationSection from '@/components/integration-section/Home6IntegrationSection'
 import Home6PartnerSection from '@/components/partner-sections/Home6PartnerSection'
@@ -11,6 +11,7 @@ import Home6processSection from '@/components/process-sections/Home6processSecti
 import Home6TestimonialSection from '@/components/testimonial-section/Home6TestimonialSection'
 import Home6PricingSection from '@/components/pricing-section/Home6PricingSection'
 import React, { useEffect } from 'react'
+import Footer7 from '@/components/Footer/Footer7'
 
 export default function Home() {
     useEffect(() => {
@@ -27,8 +28,8 @@ export default function Home() {
             <Home6processSection />
             <Home6PricingSection />
             <Home6Faq />
-            <Home6BlogSection />
-            <Footer6 />
+            
+            <Footer7/>
         </>
     )
 }
