042e5b02ceb392e6c05e389e07be6519780f22be
Jeffrey Rowlings <rawlinzjeff264@gmail.com>
2025-07-01 23:38:09 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,41 @@
 "use client"
+import React, { useEffect } from 'react'
 import Home6Banner from '@/components/banner/Home6Banner'
-import Home6BlogSection from '@/components/blog-section/Home6BlogSection'
 import Home6Faq from '@/components/faq-section/Home6Faq'
 import Home6FeatureSection from '@/components/feature-section/Home6FeatureSection'
-// import Home1Footer from '@/components/Footer/Home1Footer'
 import Header6 from '@/components/header/Header6'
 import Home6IntegrationSection from '@/components/integration-section/Home6IntegrationSection'
 import Home6PartnerSection from '@/components/partner-sections/Home6PartnerSection'
 import Home6processSection from '@/components/process-sections/Home6processSection'
 import Home6TestimonialSection from '@/components/testimonial-section/Home6TestimonialSection'
 import Home6PricingSection from '@/components/pricing-section/Home6PricingSection'
-import React, { useEffect } from 'react'
 import Footer7 from '@/components/Footer/Footer7'
 
 export default function Home() {
     useEffect(() => {
         document.body.classList.add("digital-agency");
-    })
+
+        // Inject Calendly badge widget script
+        const script = document.createElement("script");
+        script.src = "https://assets.calendly.com/assets/external/widget.js";
+        script.async = true;
+        script.onload = () => {
+            if (window.Calendly) {
+                window.Calendly.initBadgeWidget({
+                    url: 'https://calendly.com/krepsoftware/krep-demo-meeting?text_color=134a2f&primary_color=17533a',
+                    text: 'Schedule time with me',
+                    color: '#035b20',
+                    textColor: '#ffffff',
+                });
+            }
+        };
+        document.head.appendChild(script);
+
+        return () => {
+            document.body.classList.remove("digital-agency");
+        };
+    }, []);
+
     return (
         <>
             <Header6 />
@@ -28,8 +47,7 @@ export default function Home() {
             <Home6processSection />
             <Home6PricingSection />
             <Home6Faq />
-            
-            <Footer7/>
+            <Footer7 />
         </>
-    )
+    );
 }
